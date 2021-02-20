39f1ad8e8053bc86cef78be08f0138f9c495c7c2
Aslak Frafjord Skailand <AslakSkailand@gmail.com>
2021-02-20 11:25:53 +0100

    added eslint, prettier and husky

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App/App';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import {logger} from 'redux-logger';
-import { app } from './reducers';
-import './index.css';
-import 'typeface-roboto';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App/App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { logger } from "redux-logger";
+import { app } from "./reducers";
+import "./index.css";
+import "typeface-roboto";
+import * as serviceWorker from "./serviceWorker";
 
 const store = createStore(app, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
-    <div className="frontpage">
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </div>
-    ,document.getElementById('root'));
+  <div className="frontpage">
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </div>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
